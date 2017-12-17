ffb1e771c785ddd0b0f85dc3c40c6001eb06634a
pranaykarani <pranaykarani@outlook.com>
2017-12-17 11:57:13 +0530

    Initial commit

---
+++ desktop/src/com/botxgames/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.botxgames.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.botxgames.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}
